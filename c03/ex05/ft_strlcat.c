20c73bde254518180fa721fdb485793b5cc2930b
IbraSenpai <108842434+IbraSenpai@users.noreply.github.com>
2022-07-15 11:05:53 +0200

    Update ft_strlcat.c

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,14 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ibah <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/14 15:22:20 by ibah              #+#    #+#             */
-/*   Updated: 2022/07/14 16:25:28 by ibah             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+//#include <stdio.h>
 
 int	ft_strlen(char *tab)
 {
@@ -26,17 +16,29 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i_dest;
 	unsigned int	i_src;
+    unsigned int    len_dest;
+    unsigned int    len_src;
 
 	i_dest = ft_strlen(dest);
-	i_src = 0;
-	if (!src[i_src])
-		return (&dest);
-	while ((src[i_sr]) && (i_src < size -1))
+    i_src = 0;
+    len_dest = i_dest;
+	len_src = ft_strlen(src);
+	if ((size <= len_dest) || (size == 0))
+		return (len_src + size);
+	while ((src[i_src]) && (i_src < size - len_dest - 1))
 	{
 		dest[i_dest] = src[i_src];
 		i_src++;
 		i_dest++;
 	}
 	dest[i_dest] = '\0';
-	return (&dest);
+	return (len_src + len_dest);
 }
+
+/*int main (void)
+{
+	char src[] = "ibrahim";
+    	char dest [] = "01";
+    printf("%i \n", ft_strlcat(dest, src, 15));
+    printf("%s \n", dest);
+}*/	
